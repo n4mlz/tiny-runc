1d937f1f972a12a78907f326906e689d30d8370c
n4mlz <n4mlz.dev@gmail.com>
2024-12-13 19:00:03 +0900

    :memo: tiny-runc の説明を help に追加

--- cmd/root.go
+++ cmd/root.go
@@ -6,23 +6,31 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "tiny-runc",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "Open Container Initiative runtime",
+	Long: `tiny-runc (WIP)
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+This is a lightweight version of the low-level container runtime, runc (WIP).
+
+It aims to serve as a reference material for understanding how container runtimes operate, rather than focusing on practical use.
+
+The following goals are set:
+- Compliance with the essential parts of the OCI Runtime Specification
+- **Rootless operation**
+- Simple and easy-to-understand code
+- Serve as a reference for creating custom container runtimes
+
+Conversely, the following are not within the scope of the goals:
+- Full compliance with the OCI Runtime Specification
+- Advanced security considerations
+- Use in production environments
+- Integration with high-level container runtimes
+
+License:
+This project is licensed under the MIT License.`,
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -31,13 +39,4 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.tiny-runc.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
