90675906435419f795efb2537fe37be1bd38cb7d
n4mlz <n4mlz.dev@gmail.com>
2024-12-13 19:21:01 +0900

    :zap: env, cwd, args に対応

--- lib/init_3.go
+++ lib/init_3.go
@@ -30,13 +30,15 @@ func Init_3(containerID string, bundlePath string) {
 		}
 	}
 
-	// TODO: read from config
-	cmd := exec.Command("/bin/sh")
+	cmd := exec.Command(c.Process.Args[0], c.Process.Args[1:]...)
+	cmd.Env = c.Process.Env
+	cmd.Dir = c.Process.Cwd
 
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
+	// TODO: wait for start signal
 	if err := cmd.Run(); err != nil {
 		log.Fatalf("failed to run shell: %v", err)
 	}
