df8d026d081bc62365d51f93f5025f0a8a08d3d3
n4mlz <n4mlz.dev@gmail.com>
2024-12-17 15:20:58 +0900

    :zap: 0 以外の uid/gid に対応

--- lib/init_3.go
+++ lib/init_3.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"syscall"
 )
 
 func Init_3(containerID string) {
@@ -21,12 +22,18 @@ func Init_3(containerID string) {
 	Uts(config)
 
 	cmd := exec.Command(config.Process.Args[0], config.Process.Args[1:]...)
+
 	cmd.Env = config.Process.Env
 	cmd.Dir = config.Process.Cwd
-
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Credential: &syscall.Credential{
+			Uid: config.Process.User.UID,
+			Gid: config.Process.User.GID,
+		},
+	}
 
 	// TODO: wait for start signal
 	if err := cmd.Run(); err != nil {
